0c9822808c273252637456613cd796fba543e1ac
raphael.attias <raphael.attias@laplateforme.io>
2024-01-31 18:27:31 +0100

    job08

--- jour02/job08.py
+++ jour02/job08.py
@@ -1,2 +1,138 @@
 import mysql.connector
 
+def initialiser_base_de_donnees():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="R@ph@e?13*?",
+        database="zoo"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS animal (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nom VARCHAR(255) NOT NULL,
+            race VARCHAR(255) NOT NULL,
+            id_cage INT,
+            date_naissance DATE NOT NULL,
+            pays_origine VARCHAR(255) NOT NULL,
+            FOREIGN KEY (id_cage) REFERENCES cage(id)
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cage (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            superficie FLOAT NOT NULL,
+            capacite_max INT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def ajouter_animal(nom, race, id_cage, date_naissance, pays_origine):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="R@ph@e?13*?",
+        database="zoo"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO animal (nom, race, id_cage, date_naissance, pays_origine)
+        VALUES (%s, %s, %s, %s, %s)
+    ''', (nom, race, id_cage, date_naissance, pays_origine))
+
+    conn.commit()
+    conn.close()
+
+def ajouter_cage(superficie, capacite_max):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="R@ph@e?13*?",
+        database="zoo"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO cage (superficie, capacite_max)
+        VALUES (%s, %s)
+    ''', (superficie, capacite_max))
+
+    conn.commit()
+    conn.close()
+
+def afficher_animaux():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="R@ph@e?13*?",
+        database="zoo"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT * FROM animal
+    ''')
+    animaux = cursor.fetchall()
+
+    for animal in animaux:
+        print(animal)
+
+    conn.close()
+
+def afficher_animaux_par_cage():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="R@ph@e?13*?",
+        database="zoo"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT c.id, c.superficie, c.capacite_max, a.*
+        FROM cage c
+        LEFT JOIN animal a ON c.id = a.id_cage
+    ''')
+    animaux_par_cage = cursor.fetchall()
+
+    for animal_cage in animaux_par_cage:
+        print(animal_cage)
+
+    conn.close()
+
+def calculer_superficie_totale():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="R@ph@e?13*?",
+        database="zoo"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT SUM(superficie) FROM cage
+    ''')
+    superficie_totale = cursor.fetchone()[0]
+
+    print("Superficie totale des cages :", superficie_totale)
+
+    conn.close()
+
+initialiser_base_de_donnees()
+
+ajouter_cage(10.5, 5)
+ajouter_cage(15.0, 8)
+
+ajouter_animal("Chat", "Félin", 1, "2020-01-15", "Afrique")
+ajouter_animal("Girafe", "Mammifère", 2, "2019-05-20", "Afrique")
+ajouter_animal("Panda", "Ours", 2, "2021-03-10", "Chine")
+
+afficher_animaux()
+afficher_animaux_par_cage()
+calculer_superficie_totale()
--- jour02/job08.sql
+++ jour02/job08.sql
@@ -1,2 +1,27 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+DROP TABLE IF EXISTS animal;
+CREATE TABLE `animal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `race` varchar(255) NOT NULL,
+  `id_cage` int DEFAULT NULL,
+  `date_naissance` date NOT NULL,
+  `pays_origine` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cage` (`id_cage`),
+  CONSTRAINT `animal_ibfk_1` FOREIGN KEY (`id_cage`) REFERENCES `cage` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-USE zoo;
+DROP TABLE IF EXISTS cage;
+CREATE TABLE `cage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `superficie` float NOT NULL,
+  `capacite_max` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO animal(id,nom,race,id_cage,date_naissance,pays_origine) VALUES('2','\'chat\'','\'Félin\'','1','\'2020-01-15\'','\'Afrique\''),('3','\'Girafe\'','\'Mammifère\'','2','\'2019-05-20\'','\'Afrique\''),('4','\'Panda\'','\'Ours\'','2','\'2021-03-10\'','\'Chine\''),('5','\'Chat\'','\'Félin\'','1','\'2020-01-15\'','\'Afrique\''),('6','\'Girafe\'','\'Mammifère\'','2','\'2019-05-20\'','\'Afrique\''),('7','\'Panda\'','\'Ours\'','2','\'2021-03-10\'','\'Chine\'');
+INSERT INTO cage(id,superficie,capacite_max) VALUES('1','10.5','5'),('2','15','8'),('3','10.5','5'),('4','15','8');
